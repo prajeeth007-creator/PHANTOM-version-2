00dc39a963e74f9b1c6b7dbb78fdfaeed5fa81fa
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-03-07 13:24:51 +0530

    files added and modified

--- index.py
+++ index.py
@@ -1,36 +1,16 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
 from flask import Flask, render_template, request
+from brain import get_response
 
 app = Flask(__name__)
-
-# Initialize BMO
-bot = ChatBot(
-    "cooper",
-    logic_adapters=[
-        {
-            "import_path": "chatterbot.logic.BestMatch",
-            "default_response": "cooper does not compute!",
-            "maximum_similarity_threshold": 0.90
-        }
-    ]
-)
-
-trainer = ChatterBotCorpusTrainer(bot)
-trainer.train("chatterbot.corpus.english")
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/get")
-def get_response():
-    # This 'userMessage' must match the JS fetch URL
-    user_text = request.args.get("userMessage") 
-    if user_text:
-        return str(bot.get_response(user_text))
-    return "I didn't hear you!"
+def get_bot_response():
+    user_text = request.args.get("userMessage")
+    return get_response(user_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
- 
